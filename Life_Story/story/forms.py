77b0721cba3e853a16be6307367dc818ef108cd4
Shakil526563 <mdshakil526563@gmail.com>
2022-11-03 07:41:42 +0600

    first commit

---
+++ Life_Story/story/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Profile, BlogPost,storyPost
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('phone_no', 'bio', 'facebook', 'instagram', 'linkedin', 'image', )
+     
+        
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'slug', 'content', 'image')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Blog Titile'}),
+            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Copy the title with no space and a hyphen in between'}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Content of the Blog'}),
+        }
+class BlogPostForm1(forms.ModelForm):
+    class Meta:
+        model = storyPost
+        fields = ('title', 'slug', 'content', 'image')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Blog Titile'}),
+            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Copy the title with no space and a hyphen in between'}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Content of the Blog'}),
+        }
